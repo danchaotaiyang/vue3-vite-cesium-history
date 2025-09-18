f3c76a3867138f7721e2b443825f14be47fbab7a
danchaotaiyang <yuxiaolong00@qq.com>
2025-09-18 16:37:29 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,8 @@
 </template>
 
 <style lang="scss">
-@use 'assets/css/root.scss';
-@use 'assets/css/redefine.scss';
 @use 'element-plus/dist/index.css';
 @use 'assets/css/element-plus.scss';
+@use 'assets/css/redefine.scss';
 @use 'assets/css/layout.scss';
 </style>
