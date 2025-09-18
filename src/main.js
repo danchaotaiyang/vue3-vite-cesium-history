860f875bef89650576a9af8c20717ceacc96f76e
danchaotaiyang <yuxiaolong00@qq.com>
2025-09-18 16:42:47 +0800

    .

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue';
 import router from './router';
 import pinia from './store';
 import App from './App.vue';
-import ElementPlus from 'element-plus';
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 import Components from './components';
 import Directives from './directive';
+import ElementPlus from 'element-plus';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 
 
 const main = createApp(App);
