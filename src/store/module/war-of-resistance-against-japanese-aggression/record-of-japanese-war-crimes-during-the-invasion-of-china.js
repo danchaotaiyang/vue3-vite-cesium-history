7873000e400461f26231ecdb3e733ea70dcfcc4d
danchaotaiyang <yuxiaolong00@qq.com>
2025-09-23 02:24:47 +0800

    .

--- src/store/module/war-of-resistance-against-japanese-aggression/record-of-japanese-war-crimes-during-the-invasion-of-china.js
+++ src/store/module/war-of-resistance-against-japanese-aggression/record-of-japanese-war-crimes-during-the-invasion-of-china.js
@@ -4,6 +4,8 @@ import * as Cesium from 'cesium';
 import { cameraView } from '@/config.js';
 
 
+const mapboxAccessToken = 'pk.eyJ1IjoieXV4aWFvbG9uZzAyMSIsImEiOiJjbWZ2ZjlhdWMwNnI1Mm5wdDYzdTR1eWpiIn0.2eGlwGPf3LCBCUHWZ5EPnw';
+
 export const useRecordOfJapaneseWarCrimesDuringTheInvasionOfChinaStore = defineStore('record-of-japanese-war-crimes-during-the-invasion-of-china', () => {
 
     const id = 'record-of-japanese-war-crimes-during-the-invasion-of-china';
@@ -20,13 +22,29 @@ export const useRecordOfJapaneseWarCrimesDuringTheInvasionOfChinaStore = defineS
             navigationHelpButton: false,
             sceneModePicker: false,
             selectionIndicator: false,
+            terrain: Cesium.Terrain.fromWorldTerrain({
+                requestVertexNormals: true,
+                requestWaterMask: true
+            })
         });
         viewer[ '_cesiumWidget' ][ '_creditContainer' ][ 'style' ][ 'display' ] = 'none';
         viewer[ '_toolbar' ][ 'style' ][ 'display' ] = 'none';
         viewer.camera.setView({
             destination: Cesium.Cartesian3.fromDegrees(cameraView.longitude, cameraView.latitude, cameraView.height)
         });
-        // viewer.scene.imageryLayers.removeAll(true);
+
+
+
+        const providerAsync = (option) => new Promise((resolve) => {
+            resolve(new Cesium.UrlTemplateImageryProvider(option));
+        });
+
+        const option = {
+            url: `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${ mapboxAccessToken }`
+        };
+        const layer = Cesium.ImageryLayer.fromProviderAsync(providerAsync(option), null);
+        viewer.scene.imageryLayers.add(layer);
+
     };
 
     const data = ref([]);
