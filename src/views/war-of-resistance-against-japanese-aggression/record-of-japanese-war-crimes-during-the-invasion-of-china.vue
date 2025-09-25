0b82c4af2284e51854962628ef553b8f35163011
danchaotaiyang <yuxiaolong00@qq.com>
2025-09-25 14:01:29 +0800

    .

--- src/views/war-of-resistance-against-japanese-aggression/record-of-japanese-war-crimes-during-the-invasion-of-china.vue
+++ src/views/war-of-resistance-against-japanese-aggression/record-of-japanese-war-crimes-during-the-invasion-of-china.vue
@@ -21,6 +21,30 @@ onMounted(() => {
 <div :id="store.id" class="width-100-v height-100-v"></div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.cesium-timeline-ticTiny, .cesium-timeline-ticSub {
+    display: none;
+}
+/* 隐藏小时级别的刻度线 */
+.cesium-timeline-ticks .cesium-timeline-tick:not(.cesium-timeline-tickMajor) {
+    display: none;
+}
 
+/* 或者更精确地控制显示 */
+.cesium-timeline-ticks .cesium-timeline-tickHour {
+    display: none;
+}
+
+.cesium-timeline-ticks .cesium-timeline-tickMinute {
+    display: none;
+}
+
+.cesium-timeline-ticks .cesium-timeline-tickSecond {
+    display: none;
+}
+
+/* 只显示主要的刻度线（通常是天级别） */
+.cesium-timeline-ticks .cesium-timeline-tickMajor {
+    display: block;
+}
 </style>
