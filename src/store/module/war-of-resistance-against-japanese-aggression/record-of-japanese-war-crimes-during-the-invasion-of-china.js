b9835eefaa00f700dc80d42bd68d181ee31ecba8
danchaotaiyang <yuxiaolong00@qq.com>
2025-09-25 10:47:41 +0800

    .

--- src/store/module/war-of-resistance-against-japanese-aggression/record-of-japanese-war-crimes-during-the-invasion-of-china.js
+++ src/store/module/war-of-resistance-against-japanese-aggression/record-of-japanese-war-crimes-during-the-invasion-of-china.js
@@ -1,8 +1,14 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import * as Cesium from 'cesium';
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+import 'dayjs/locale/zh-cn';
 import { cameraView } from '@/config.js';
+import data from './record-of-japanese-war-crimes-during-the-invasion-of-china.json';
 
+dayjs.extend(weekday);
+dayjs.locale('zh-cn');
 
 const mapboxAccessToken = 'pk.eyJ1IjoieXV4aWFvbG9uZzAyMSIsImEiOiJjbWZ2ZjlhdWMwNnI1Mm5wdDYzdTR1eWpiIn0.2eGlwGPf3LCBCUHWZ5EPnw';
 
@@ -12,7 +18,9 @@ export const useRecordOfJapaneseWarCrimesDuringTheInvasionOfChinaStore = defineS
 
     let viewer;
 
-    const createCesium = () => {
+    const currentNode = ref(0);
+
+    const createCesium = async () => {
         viewer = new Cesium.Viewer(id, {
             animation: false,
             baseLayerPicker: false,
@@ -29,25 +37,63 @@ export const useRecordOfJapaneseWarCrimesDuringTheInvasionOfChinaStore = defineS
         });
         viewer[ '_cesiumWidget' ][ '_creditContainer' ][ 'style' ][ 'display' ] = 'none';
         viewer[ '_toolbar' ][ 'style' ][ 'display' ] = 'none';
+
         viewer.camera.setView({
             destination: Cesium.Cartesian3.fromDegrees(cameraView.longitude, cameraView.latitude, cameraView.height)
         });
 
+        await addImageryLayerMapbox();
 
+        setTimeline();
 
-        const providerAsync = (option) => new Promise((resolve) => {
-            resolve(new Cesium.UrlTemplateImageryProvider(option));
-        });
-
-        const option = {
-            url: `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${ mapboxAccessToken }`
-        };
-        const layer = Cesium.ImageryLayer.fromProviderAsync(providerAsync(option), null);
-        viewer.scene.imageryLayers.add(layer);
+        setCurrentEvent();
 
     };
 
-    const data = ref([]);
+    const setTimeline = () => {
+        viewer.timeline.makeLabel = (time) => dayjs(Cesium.JulianDate.toDate(time)).format('YYYY-MM-DD dddd');
+    };
+
+    const addImageryLayerMapbox = async () => {
+
+        try {
+
+            let provider = new Cesium.UrlTemplateImageryProvider({
+                url: `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${ mapboxAccessToken }`
+            });
+
+            const layer = await Cesium.ImageryLayer.fromProviderAsync(Promise.resolve(provider), null);
+
+            viewer.scene.imageryLayers.add(layer);
+
+        } catch (e) {
+            console.warn(e);
+        }
+    };
+
+    const setCurrentEvent = () => {
+
+        if (data.length === 0) {
+            return;
+        }
+
+        try {
+
+            let currentTime = Cesium.JulianDate.fromDate(new Date(data[ currentNode.value ].time));
+            let startTime = Cesium.JulianDate.addDays(currentTime.clone(), -2, new Cesium.JulianDate());
+            let stopTime = Cesium.JulianDate.addDays(currentTime.clone(), 2, new Cesium.JulianDate());
+
+            if (startTime && stopTime) {
+
+                viewer.timeline.zoomTo(startTime, stopTime);
+                viewer.clock.startTime = startTime.clone();
+                viewer.clock.stopTime = stopTime.clone();
+                viewer.clock.currentTime = currentTime.clone();
+            }
+        } catch (e) {
+            console.warn(e);
+        }
+    };
 
     return { id, createCesium };
 }, {
